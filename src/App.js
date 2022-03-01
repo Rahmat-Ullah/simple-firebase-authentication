ccdf59a41906be1d68cc1446c6c894614580528e
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-03-01 13:30:59 +0600

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import {getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+
+
+initializeAuthentication();
+
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({})
+
+
+  const handleGoogleSignIn = () => {
+    const auth = getAuth()
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form className="main_div" action="allcategories.html">
+      <h2 className="title">Login Form</h2>
+            <div class="input_box">
+              <input type="text" id="username" placeholder="Username"></input>
+              <div class="icon"><i class="fas fa-user"></i></div>
+            </div>
+            <div class="input_box">
+                <input type="password" id="password" placeholder="Password"></input>
+                <div class="icon"><i class="fas fa-lock"></i></div>
+            </div>
+            <div class="input_box button">
+                <input type="submit" value="Login"></input>
+            </div>
+        </form>
+      <button class="googlebtn" onClick={handleGoogleSignIn}>Login with google</button>
+      <br />
+      {
+        user.email && <div>
+          <h2>Welcome {user.name}</h2>
+          {/* <h2>I know your email address: {user.email}</h2> */}
+        </div>
+      }
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDLZVQ0MkX5zb-JkYiu0e6HkdwRf_ALaE",
+    authDomain: "simple-firebase-authenti-161d4.firebaseapp.com",
+    projectId: "simple-firebase-authenti-161d4",
+    storageBucket: "simple-firebase-authenti-161d4.appspot.com",
+    messagingSenderId: "803022446292",
+    appId: "1:803022446292:web:0dfa54890a1f3469dce541"
+  };
+
+  export default firebaseConfig;
